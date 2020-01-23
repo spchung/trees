036914c62ef97d2965fc64cb475bd38540fda713
Stephen Chung <stephen@campus-043-199.ucdavis.edu>
2020-01-23 14:57:29 -0800

    Added Slider

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Slider from './Slider';
 
 
 class Canvas extends React.Component{
@@ -9,22 +10,26 @@ class Canvas extends React.Component{
             receivedData : false,
         } 
     }
+    // update local state from props changes 
     static getDerivedStateFromProps(props, state) {
         return {receivedData: props.received };
-      }
-
-    onComponentDidmount(){
-        let txt ="";
-        if(this.state.receivedData){
-            txt = "yeer"
-        }else{
-            txt = "naaar"
-        }
-        console.log(txt);
-        this.setState({
-            receivedData : this.props.received,
-        });
     }
+
+    componentDidMount(){
+        const canvas = this.refs.canvas;
+        this.ctx = canvas.getContext("2d");
+
+        // resize
+        window.addEventListener('resize', this.onWindowResize, false); 
+    }
+
+    onWindowResize = () => {
+        this.ctx.canvas.width = window.innerWidth;
+        this.ctx.canvas.height = window.innerHeight*0.8;
+        console.log("Height: ", window.innerHeight)
+        console.log("canvas height: ", this.ctx.canvas.height)
+    }
+
 
     render(){
         let para;
@@ -37,8 +42,16 @@ class Canvas extends React.Component{
         }
         return(
             <div>
-                <canvas ref="canvas" color="red"/>
+                <canvas ref="canvas" width={window.innerWidth} height={(window.innerHeight*0.8)} />
                 {para}
+
+                <Slider
+                    initial={0}
+                    max={100} // use length of vector 
+                    formatFn={number => number.toFixed(2)}
+                    onChange={value => console.log(value)} // round value to get index for treeVect
+                />
+
             </div>
         )
     }
